29ced3ce729a8d43242268b6c900a5a198d49391
Daniel Lessa <62571768+Danielessa@users.noreply.github.com>
2024-07-24 08:14:01 -0400

    Update conta-bancaria.py

--- conta-bancaria.py
+++ conta-bancaria.py
@@ -9,25 +9,35 @@ class ContaBancaria:
         self.contas.append(self)
         proxima_acao()
 
-    def verificar_conta():
+    def encontrar_conta():
         titular = input('Quem é o titular da conta?\n')
-        
         for usuario in ContaBancaria.contas:
             if usuario.titular == titular:
-                print(f'N_conta: {usuario.N_conta}')
-                print(f'Titular: {usuario.titular}')
-                print(f'Saldo: {usuario.saldo}')
+                return usuario
+
+
+    def verificar_conta():
+
+        usuario = ContaBancaria.encontrar_conta()
+        
+        print(f'N_conta: {usuario.N_conta}')
+        print(f'Titular: {usuario.titular}')
+        print(f'Saldo: {usuario.saldo}')
         
         proxima_acao()
     
-    def depositar(self, v_deposito):
+    def depositar(v_deposito):
+        usuario = ContaBancaria.encontrar_conta()
+
         print(f'Foi depositado R$ {v_deposito} na sua conta')
-        self.saldo += v_deposito
+        usuario.saldo += v_deposito
         proxima_acao()
 
-    def sacar(self, v_saque):
+    def sacar(v_saque):
+        usuario = ContaBancaria.encontrar_conta()
+
         print(f'Foi sacado R$ {v_saque} da sua conta')
-        self.saldo -= v_saque
+        usuario.saldo -= v_saque
         proxima_acao()
 
 def menu():
@@ -50,10 +60,12 @@ def menu():
         usuario = ContaBancaria(ncont, n, saldo)
 
     elif a == '3':
-        ContaBancaria.depositar()
+        deposito = int(input('Quanto deseja depositar?\n'))
+        ContaBancaria.depositar(deposito)
 
     elif a == '4':
-        ContaBancaria.depositar()
+        saque = int(input('Quanto deseja sacar?\n'))
+        ContaBancaria.sacar(saque)
 
 def proxima_acao():
     a = input('Deseja voltar ao menu?[s/n]\n')
