433f57f43acc9c7358ca22203ecab0a12f51bd9f
Daniel Lessa <62571768+Danielessa@users.noreply.github.com>
2024-09-05 05:05:16 -0400

    v2

--- python-orientado-objeto/teste.py
+++ python-orientado-objeto/teste.py
@@ -1,16 +1,9 @@
-class Temperatura:
-    def __init__(self, celsius):
-        self._celsius = celsius
+list_numeros = []
+for n in range(1, 1065, 1):
+    aux_n = n
+    aux_n = str(aux_n)
+    list_numeros.append(f'{aux_n.ljust(8)}')
 
-    @property
-    def celsius(self):
-        return self._celsius
-
-    @property
-    def fahrenheit(self):
-        return (self._celsius * 9/5) + 32
-
-# Teste
-temp = Temperatura(25)
-print(f'Temperatura em Celsius: {temp.celsius}°C')
-print(f'Temperatura em Fahrenheit: {temp.fahrenheit}°F')
+print(list_numeros)
+with open('numeros.txt', 'w') as numeros:
+    numeros.writelines(list_numeros)
